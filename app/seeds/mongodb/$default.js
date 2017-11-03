359fc2c470880c4197f1ae86cfa7e42b94dc8d9a
Nate Rupsis <nrupsis@gmail.com>
2017-11-02 21:32:47 -0400

    updated db seed to include dogs

--- app/seeds/mongodb/$default.js
+++ app/seeds/mongodb/$default.js
@@ -4,18 +4,38 @@ const dab = require ('@onehilltech/dab');
 
 // stubbing out the basic user profiles
 module.exports = {
+
+
+
   users: dab.times (5, function (i, opts, callback) {
-    return callback (null, {
-        firstName: 'firsty Namey ' + i,
-        lastName: 'lasty Namey' + i,
-        email: 'accounty' + i + '@email.com',
-        password: 'passwordy' + i,
-        gender: 'maleOrFemaleorShemale' + i,
-        bio: 'biographies' + i,
-        homeAddress: 'homeAddressess' + i,
-        geoLocation: "geoLoct" + i,
-        birthday: "day of birth" + i,
-        numOfDogs: 13 + i
-    });
+
+      var times = Math.floor(Math.random() * 4) + 1;
+
+      return callback(null, {
+          firstName: 'firsty Namey ' + i,
+          lastName: 'lasty Namey' + i,
+          email: 'account' + i + '@email.com',
+          password: 'password' + i,
+          gender: 'maleOrFemaleorShemale' + i,
+          bio: 'biographies' + i,
+          homeAddress: 'homeAddressess' + i,
+          geoLocation: "geoLoct" + i,
+          status: 'status' + i,
+          birthday: Date.UTC((1990 + i), (0 + 1), i, 0, 0, 0),
+          dog: dab.times(times, function(i, opts, callback){
+              return callback(null, {
+                  firstName: 'first dogo name' + i,
+                  lastName: 'lasty dogo name' + i,
+                  bio: 'im heckin cool',
+                  gender:'maleOrFemaleorShemale',
+                  breed: 'breed' + i,
+                  birthday: Date.UTC((2005 + i), (1 + 1), i, 0, 0, 0),
+                  size:"large",
+                  fixed:"neutered/spayed",
+                  vetVerification:["verification"]
+              });
+          })
+
+      });
   })
 };
