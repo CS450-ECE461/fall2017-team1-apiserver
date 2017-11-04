0ef9272ef2dc6ca3fe5f4f59cd4a11dd3f3a55e8
singh65 <singh65@umail.iu.edu>
2017-11-04 03:50:13 -0400

    Able to create new user, only inserts in dummy data atm

--- app/controllers/RegController.js
+++ app/controllers/RegController.js
@@ -1,6 +1,7 @@
 var blueprint = require ('@onehilltech/blueprint'),
     mongodb = ('@onehilltech/blueprint-mongodb'),
     ResourceController = mongodb.ResourceController,
+    HttpError = blueprint.errors.HttpError,
     User = require('../models/User'),
     util = require ('util'),
     dab = require ('@onehilltech/dab');
@@ -17,67 +18,71 @@ blueprint.controller (RegController, blueprint.ResourceController);
 
 
 RegController.prototype.createUser = function(){
-    return function(req, res) {
-        var fn = req.body.FirstName;
-        var ln= req.body.LastName;
-        var em = req.body.Email;
-        var pw = req.body.Password;
-        console.log('---------------------------------------- 1');
+    var self = this;
 
-        var data = {
+    console.log('---------------------------------------- 1');
 
-            users: dab.times(1, function (data, opts, callback) {
-                return callback(null, {
+    return {
+        execute: function (req, res, callback) {
+            var fn = req.body.FirstName;
+            var ln = req.body.LastName;
+            var em = req.body.Email;
+            var pw = req.body.Password;
+            var newUser = new User({
 
-                    firstName: fn,
-                    lastName: ln,
-                    email: em,
-                    password: pw,
+                firstName: 'N/A',
+                lastName: 'N/A',
+                email: 'N/A',
+                password: 'N/A',
 
-                    gender: 'N/A',
+                gender: 'N/A',
+
+                bio: 'N/A',
+
+                homeAddress: 'N/A',
+
+                status: 'N/A',
+
+                birthday: Date.UTC((1970), (0), 0, 0, 0, 0),
+
+                activated: false,
+
+                dog: [{
+                    firstName: 'N/A',
+
+                    lastName: 'N/A',
 
                     bio: 'N/A',
 
-                    homeAddress: 'N/A',
+                    gender: 'N/A',
 
-                    status: 'N/A',
+                    breed: 'N/A',
 
                     birthday: Date.UTC((1970), (0), 0, 0, 0, 0),
 
-                    activated: false,
+                    size: 'N/A',
 
-                    dog: [{
-                        firstName: 'N/A',
+                    fixed: 'N/A',
 
-                        lastName: 'N/A',
+                    vetVerification: ['N/A']
+                }]
+            });
+            newUser.save(function(err, newUser) {
+                if (err) return callback(new HttpError (500, 'Failed to save user'));
 
-                        bio: 'N/A',
-
-                        gender: 'N/A',
-
-                        breed: 'N/A',
-
-                        birthday: Date.UTC((1970), (0), 0, 0, 0, 0),
-
-                        size: 'N/A',
-
-                        fixed: 'N/A',
-
-                        vetVerification: ['N/A']
-                    }]
-                })
-
-            })
+            res.status(200).json(newUser.id);
+            return callback(null);
+            });
+            console.log('---------------------------------------- 2');
+            console.log('---------------------------------------- 3');
         }
-        console.log('---------------------------------------- 2');
-        dab.build (data, function (err, data) {
-            // data is the generated data
-        });
-        console.log('---------------------------------------- 3');
-    }
-}
+
+    };
+
+};
 
 
 
 
 module.exports = exports = RegController;
+
