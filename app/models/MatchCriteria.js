9a9984f22a34e4f506fd2e52a49ea5ba650d0ae0
singh65 <singh65@umail.iu.edu>
2017-11-14 00:22:30 -0500

    Added queue, not tests, or seed for it yet

--- app/models/MatchCriteria.js
+++ app/models/MatchCriteria.js
@@ -6,7 +6,7 @@ const blueprint = require ('@onehilltech/blueprint'),
     User = require('./User');
 
 
-// the C after variable names stands for " Criteria ". Put it there just to have seperate names from user schema.
+// the C after variable names stands for " Criteria ". Put it there just to have separate names from user schema.
 var criteriaSchema = new mongodb.Schema({
     minAgeOfDog: {type: Number, trim: true},
 
@@ -18,12 +18,19 @@ var criteriaSchema = new mongodb.Schema({
 
     statusC: {type: String, trim: true},
 
-    locationC: {type: String, trim: true}//this will change in the future once location is set up.
-    //potentialMatchesQueue :{type: [ObejectId], ref: User}
+    locationC: {type: String, trim: true},//this will change in the future once location is set up.
+
+    potentialMatchesQueue :{type: [ObjectId], ref: User}
     });
 
 
+
+
 //criteria model methods
+
+//We might not need all the rest of the getters/setters, but have them here for now.
+
+
 criteriaSchema.methods.getDogMinAge = function(){
     return this.minAgeOfDog;
 };
@@ -45,6 +52,30 @@ criteriaSchema.methods.getStatus = function(){
 };
 
 criteriaSchema.methods.getLocation = function(){
+    return this.locationC;
+};
+
+criteriaSchema.methods.setDogMinAge = function(){
+    return this.minAgeOfDog;
+};
+
+criteriaSchema.methods.setDogMaxAge = function(){
+    return this.maxAgeOfDog;
+};
+
+criteriaSchema.methods.setDogSize = function(){
+    return this.dogSizeC;
+};
+
+criteriaSchema.methods.setDogVerification = function(){
+    return this.vetVerificationC;
+};
+
+criteriaSchema.methods.setStatus = function(){
+    return this.statusC;
+};
+
+criteriaSchema.methods.setLocation = function(){
     return this.locationC;
 };
 
