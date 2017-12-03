4ff487d2d1434d5134b12f04c5d8c8d81302c01a
psingh65 <pardeep6232@gmail.com>
2017-12-02 21:29:24 -0500

    Able to upload a file, but uploads file of size 0

--- app/controllers/ImageController.js
+++ app/controllers/ImageController.js
@@ -3,12 +3,13 @@ var blueprint = require ('@onehilltech/blueprint'),
     ResourceController = mongodb.ResourceController,
     util = require ('util'),
     Image = require('../models/Image');
+var shortid = require('shortid');
 var http = require('http');
 var aws = require('aws-sdk');
 var multer  = require('multer');
-var multerS3 = require('multer-s3');
 var express = require('express');
 var app = express();
+
 
 
 function ImageController () {
@@ -20,97 +21,43 @@ blueprint.controller (ImageController, blueprint.ResourceController);
 
 
 
-// Using an express app currently to just get a simple file upload.
-// Once it works, we can replace it with a http module.
+
 ImageController.prototype.uploadImage = function () {
     return function(req, res) {
+
         console.log('---------------------------------- 1');
         aws.config.loadFromPath('app/configs/aws.config.json');
         aws.config.update({
             signatureVersion: 'v4'
         });
+        var s3 = new aws.S3();
 
         console.log('---------------------------------- 2');
         var upload = multer();
+
         console.log('---------------------------------- 2.5');
-        //functional up until here. I believe below code should go in the ImageRouter.
-        app.post("/uploadImage", multer().any(), function(req, res){
-            console.log('---------------------------------- 3');
-            var fileName = shortid.generate() + "-" + req.files[0].originalname;
-            var params = {
-                Bucket : "supdog",
-                ACL : "public-read",
-                Key : fileName,
-                Body : req.files[0].buffer
-            };
-            console.log('---------------------------------- 3.5');
-            s3.putObject(params, function(err, data) {
-                console.log('---------------------------------- 4');
-                if (err) {
-                    res.status(500).send(err)
-                }
-            });
-            console.log('---------------------------------- 5');
-            return res.status(200);
-        });
-    }
-}
-
-// This code is using multer-s3 module. Having issues after console.log 4
-/*        console.log('---------------------------------- 2');
-        var s3 = new aws.S3();
+        var fileName = shortid.generate() + "-" + req.file;
+// file : { fieldname, originalname, name,
+// encoding, mimetype, path, extension, size, truncated, buffer }
         console.log('---------------------------------- 3');
-
-        var upload = multer({
-            storage: multerS3({
-                s3: s3,
-                bucket: 'supdog',
-                acl: 'public-read-write',
-                metadata: function (req, file, cb) {
-                    cb(null, {fieldName: file.fieldname});
-                },
-                key: function (req, file, cb) {
-                    cb(null, Date.now().toString())
-                }
-            })
-        });
-
-        console.log('---------------------------------- 4');
-        app.post('/uploadImage', upload.array('photos', 3), function (req, res, next) {
-            res.send('Successfully uploaded ' + req.files.length + ' files!');
-        });
-
-        console.log('---------------------------------- 5');*/
-
-
-
-//The http module implementation
-/*        var options = {
-            host: "https://supdog.s3.amazonaws.com",
-            port: 80,//port 443 may also work
-            method: 'POST',
-            path: '/'
+        var params = {
+            Bucket : "supdog",
+            ACL : "public-read",
+            Key : fileName,
+            Body : req.file
         };
 
+        console.log('---------------------------------- 4');
+        s3.putObject(params, function(err, data) {
+            if (err) {
+                res.status(500).send(err)
+            }
+        });
 
-        callback = function(response) {
-            var str = ''
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-
-            response.on('end', function () {
-                console.log(str);
-            });
-        }
-
-        var awsRequest = http.request(options, callback);
-
-        awsRequest.write(req.files[0].buffer);
-        awsRequest.end();
-
+        console.log('---------------------------------- 5');
         return res.status(200);
-    }*/
+    }
+}
 
 
 
--- app/routers/v1/ImageRouter.js
+++ app/routers/v1/ImageRouter.js
@@ -1,14 +1,4 @@
-var blueprint = require ('@onehilltech/blueprint'),
-    util      = require ('util');
-var aws = require('aws-sdk');
-//var shortid = require('shortid');
-var multer  = require('multer');
-//var exif = require('exif').ExifImage
-//var amqp = require('amqplib/callback_api');
-var image = require('../../models/Image');
-var multerS3 = require('multer-s3');
-var express = require('express');
-var app = express();
+
 
 
 
