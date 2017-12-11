75abee40c672b32957cda95afb753ec33dfb8599
Nate Rupsis <nrupsis@gmail.com>
2017-12-10 23:06:07 -0500

    cleaned up unused registration router

--- app/routers/v1/RegRouter.js
+++
@@ -1,8 +0,0 @@
-module.exports = exports = {
-    '/reg':{
-        post : {action: 'RegController@createUser'}
-    },
-    '/reg/confirm/:emailID':{
-        post : {action: 'RegController@confirmUser'}
-    }
-};
